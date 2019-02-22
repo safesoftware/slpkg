b9a2e879c8d58fa8c19378f0afcbe42fb9516126
Joel Depooter <joel.depooter@safe.com>
2019-02-21 22:55:37 -0800

    Basic command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate structopt;
+
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+enum Settings {
+    // Packing a directory into a .slpk file is not yet implemented.
+    /*
+    #[structopt(name = "pack")]
+    Pack {
+        #[structopt(parse(from_os_str))]
+        src_dir: PathBuf
+    },
+    */
+    /// Unpacks a .slpk file into a directory
+    #[structopt(name = "unpack")]
+    Unpack {
+        /// The .slpk file which will be unpacked
+        #[structopt(parse(from_os_str))]
+        src_file: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let params = Settings::from_args();
+    println!("{:?}", params);
 }
